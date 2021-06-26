04c6a543e804e996d727231cec39a52307731ed4
Kirubanand <prdkiruba@gmail.com>
2021-06-26 22:04:28 +0530

    js added

---
+++ index.js
@@ -0,0 +1,8 @@
+function homePage(value) {
+	window.document.location.href = "./sportkit.html";
+}
+
+function sportKitPage(value) {
+	var footballKitButtons = document.querySelectorAll(".btn");
+	console.log(footballKitButtons)
+}
